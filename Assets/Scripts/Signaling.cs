a846a746eb7f1c4b4e74aa695459967cc5b3568d
Ekaterina Terenicheva <Eventcpa@gmail.com>
2020-11-19 16:43:14 +0600

    all project

---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private AudioSource _signaling;
+    [SerializeField] private float _stepDistance;
+    [SerializeField] private float _timeForWait;
+    private Coroutine _coroutine;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            _coroutine = StartCoroutine(IncreaseVolume());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StopCoroutine(_coroutine);
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            StartCoroutine(DecreaseVolume());
+        }
+    }
+
+    private IEnumerator IncreaseVolume()
+    {
+        _signaling.Play();
+        for (float i = _signaling.volume; i < _signaling.maxDistance; i += _stepDistance)
+        {
+            _signaling.volume = i;
+            yield return new WaitForSeconds(_timeForWait);
+        }
+    }
+
+    private IEnumerator DecreaseVolume()
+    {
+        for (float i = _signaling.volume; i > _signaling.minDistance; i -= _stepDistance)
+        {
+            _signaling.volume = i;
+            yield return new WaitForSeconds(_timeForWait);
+        }
+        if(_signaling.volume <= 0)
+        _signaling.Stop();
+    }
+}
---
+++ Assets/Scripts/Thief.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+
+    private void Update()
+    {
+        float key = Input.GetAxis("Horizontal");
+        if (key != 0)
+        {
+            Flip(key);
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void Flip(float key)
+    {
+        if (key > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (key < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
